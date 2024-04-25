9a1534063506a7866e7d3589f04d2c9efc3fc9bb
Vitor Veras <vitorverasm@gmail.com>
2024-04-25 18:41:50 -0300

    feat: apply sorting function

--- app/modules/order-book/state/order-book-slice.ts
+++ app/modules/order-book/state/order-book-slice.ts
@@ -1,6 +1,7 @@
 import type { PayloadAction } from '@reduxjs/toolkit'
 import { createSlice } from '@reduxjs/toolkit'
 import { BookEntry } from '../types/book-entry'
+import { sortAsks, sortBids } from '../utils/sortOrders'
 
 export interface OrderBookState {
     bids: BookEntry[],
@@ -19,8 +20,8 @@ export const orderBookSlice = createSlice({
         seedInitial: (state, action: PayloadAction<BookEntry[]>) => {
             const entries = action.payload;
 
-            state.bids = entries.filter(entry => entry.amount > 0)
-            state.asks = entries.filter(entry => entry.amount < 0)
+            state.bids = sortBids(entries.filter(entry => entry.amount > 0))
+            state.asks = sortAsks(entries.filter(entry => entry.amount < 0))
         },
     },
 })
