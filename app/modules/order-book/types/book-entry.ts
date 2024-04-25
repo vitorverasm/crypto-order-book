816395fe98f604234b9df6d63114f072d07c72a7
Vitor Veras <vitorverasm@gmail.com>
2024-04-25 16:48:30 -0300

    feat: add BookEntry type

---
+++ app/modules/order-book/types/book-entry.ts
@@ -0,0 +1,21 @@
+export type BookEntry = {
+    /**
+     * Price level
+     *
+     * Float
+     */
+    price: number,
+    /**
+     * Number of orders at that price level (delete price level if count = 0)
+     *
+     * Int
+     */
+    count: number,
+    /**
+     * Total amount available at that price level. Trading: if AMOUNT > 0 then bid else ask; Funding: if AMOUNT < 0 then bid else ask;
+     *
+     * Float
+     */
+    amount: number
+}
+
