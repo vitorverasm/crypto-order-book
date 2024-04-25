5b10f658f83786ff3e0ca71f35efc2a3edbec6b3
Vitor Veras <vitorverasm@gmail.com>
2024-04-25 18:41:31 -0300

    fix: fix parsing function

--- app/modules/order-book/utils/parseOrderBookEntry.ts
+++ app/modules/order-book/utils/parseOrderBookEntry.ts
@@ -2,10 +2,9 @@ import { formatPrice } from "../../../shared/utils/format";
 import { BookEntry } from "../types/book-entry";
 
 export function parseOrderBookEntry(rawOrderBookEntry: Array<any>): BookEntry {
-    const entry = rawOrderBookEntry[1] as string[]
     return {
-        amount: parseFloat(entry[2]),
-        count: parseFloat(entry[1]),
-        price: formatPrice(parseFloat(entry[0]), "P0"),
+        amount: parseFloat(rawOrderBookEntry[2]),
+        count: parseFloat(rawOrderBookEntry[1]),
+        price: formatPrice(parseFloat(rawOrderBookEntry[0]), "P0"),
     }
 }
